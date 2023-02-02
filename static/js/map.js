b61d512c446de8833410a05abfa7ba2a8a549850
Sch <junhg0211@gmail.com>
2023-02-02 12:44:54 +0900

    카메라 구현

--- static/js/map.js
+++ static/js/map.js
@@ -20,20 +20,70 @@ class AreaLayer {
 
     if (this.surface === null) {
       this.surface = document.createElement('canvas');
-      this.surface.width = 3840;
-      this.surface.height = 2160;
+      this.surface.width = 2048;
+      this.surface.height = 2048;
       let surfaceContext = this.surface.getContext('2d');
 
       let palette = {};
       Object.values(this.metadata).forEach(metadatum => {
         palette[metadatum.id] = metadatum.color;
       });
-      this.tree.render(surfaceContext, x, y, this.surface.width, this.surface.height, palette);
+      this.tree.render(surfaceContext, 0, 0, this.surface.width, this.surface.height, palette);
     }
 
+    context.imageSmoothingEnabled = false;
     context.drawImage(this.surface, x, y, width, height);
   }
 }
+
+function mod(x, y) {
+  x -= Math.floor(x / y) * y;
+  return x;
+}
+
+class Camera {
+  constructor() {
+    this.x = 1920;
+    this.y = 0;
+    this.zoom = 1;
+  }
+
+  getScreenX(x) {
+    return canvas.width / 2 + (x - this.x) * this.zoom;
+  }
+
+  getScreenY(y) {
+    return canvas.height / 2 + (y - this.y) * this.zoom;
+  }
+
+  getScreenLength(length) {
+    return length * this.zoom;
+  }
+
+  getCameraX(x) {
+    return (x - canvas.width / 2) / this.zoom - x;
+  }
+
+  getCameraY(y) {
+    return (y - canvas.height / 2) / this.zoom - y;
+  }
+
+  scroll(event) {
+    if (event.altKey) {
+      this.zoom *= Math.exp(-event.deltaY / 500);
+      this.zoom = Math.min(Math.max(this.zoom, 0.1), 200);
+      return;
+    }
+
+    this.x += event.deltaX / this.zoom / 2;
+    this.y += event.deltaY / this.zoom / 2;
+
+    this.x = mod(this.x, 3840)
+    this.y = Math.min(Math.max(this.y, -1080), 1080)
+  }
+}
+
+let camera = new Camera();
 
 let canvas;
 let context;
@@ -94,16 +144,22 @@ function ready() {
 function tick() {
 }
 
+function renderBackground() {
+  context.fillStyle = '#2c2c2c';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 function renderAreas() {
   areas.forEach(area => {
-    area.render(0, 0, 384, 216);
-  })
+    area.render(camera.getScreenX(0), camera.getScreenY(-1080), camera.getScreenLength(3840), camera.getScreenLength(2160));
+  });
 }
 
 /*
  * 화면 출력
  */
 function render() {
+  renderBackground();
   renderAreas();
 }
 
@@ -123,8 +179,9 @@ function resize() {
   canvas.width = window.innerWidth - toolsFrame.clientWidth - optionsFrame.clientWidth;
   canvas.height = window.innerHeight;
 
-  context.fillStyle = '#2c2c2c';
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  renderBackground();
 }
 
 window.addEventListener('resize', resize);
+
+window.addEventListener('wheel', e => camera.scroll(e));
